745f964700307faa1ba6529198877198f2b24af7
mehmetgurcan <117164931+mehmetgurcan@users.noreply.github.com>
2023-03-12 16:27:46 +0300

    time counter added

--- src/teamDatas/QuizPlayers.js
+++ src/teamDatas/QuizPlayers.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { getHours } from "../components/TimeCounter";
 
-import { barcelona } from "./LaLiga";
+import { bayern_munih } from './Bundesliga'; 
+import { atletico_de_madrid } from "./LaLiga";
+import { marseille } from "./Ligue1";
+import { liverpool, chelsea } from "./PremierLeague";
+import { napoli } from "./SerieA";
 
-const quizPlayer = barcelona[6];
+import { sivasspor, kasimpasa, fenerbahce, galatasaray, basaksehir } from "./SuperLig";
+const quizPlayersBig5League = [
+    {...bayern_munih[2], photo: 'https://img.fcbayern.com/image/upload/t_cms-portrait/f_auto/w_510,c_fill/q_auto/v1659953193/cms/public/images/fcbayern-com/players/spielerportraits/ganzkoerper/matthijs-de-ligt.png'},
+    {...atletico_de_madrid[16], photo: 'https://img-estaticos.atleticodemadrid.com/system/fotos/13121/destacado_460x460/_0022_RECORTES_WEB_0003_DE-PAUL.png?1660404527'},
+    {...marseille[17], photo: 'https://www.ligue1.com/-/media/Project/LFP/shared/Images/Players/2022-2023/57/62557.jpg'},
+    {...liverpool[12], photo: 'https://media02.tr.beinsports.com/img/players/P40083.png'},
+    {...napoli[2], photo: 'https://www.footballdatabase.eu/images/photos/players/a_315/315243.jpg'},
+    {...chelsea[8], photo: 'https://img.chelseafc.com//image/upload/f_auto,h_860,q_50/editorial/people/first-team/2022-23/04519_Play_Predictor_Header_3333x5000_Enzo.png'},
+];
 
-export default quizPlayer;
+console.log("aa:", quizPlayersBig5League[getHours() % 6])
+
+const quizPlayersSuperLig = [
+    {...sivasspor[10], photo: 'https://media02.tr.beinsports.com/img/players/P2016.png'},
+    {...kasimpasa[5], photo: 'https://media02.tr.beinsports.com/img/players/P2601.png'},
+    {...fenerbahce[7], photo: 'https://media02.tr.beinsports.com/img/players/P70692.png'},
+    {...galatasaray[24], photo: 'https://pbs.twimg.com/media/FoXewzBXgAM5TJl?format=jpg&name=large'},
+    {...basaksehir[5], photo: 'https://media02.tr.beinsports.com/img/players/P5416.png'},
+    {...chelsea[8], photo: 'https://img.chelseafc.com//image/upload/f_auto,h_860,q_50/editorial/people/first-team/2022-23/04519_Play_Predictor_Header_3333x5000_Enzo.png'},
+];
+
+export const quizPlayerBig5Leage = quizPlayersBig5League[getHours() % 6];
+export const quizPlayerSuperLig = quizPlayersSuperLig[getHours() % 6];
