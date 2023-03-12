571144a9ed89f1aeb5d42d976260b04f7fe1ceae
musaustundag <musaustundag13@gmail.com>
2023-03-12 15:44:38 +0300

    navigation bar updated

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,20 +2,23 @@ import React from "react";
 import { Link } from 'react-router-dom';
 export const Navbar = () =>{
     return (
-        <nav className="bg-siyah shadow-md px-10 py-5">
-          <div className="container mx-auto md:flex md:items-center">
-          <Link to="/"> 
-          <button onClick={() => window.location.reload(false)} className="hover:bg-gray text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
-          Find Me
+        <nav className="bg-siyah shadow-md px-10 py-5">  
+          <div className="container mx-auto md:flex space-x-5 md:items-center">
+            <Link to="/big5Teams"> 
+                <button 
+                    className="hover:bg-gray text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+                    Big Five
+                </button>
+            </Link> 
+
+            <Link to="/superLig"> 
+                <button 
+                    className="hover:bg-gray text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+                    Süper Lig
+                </button>
+            </Link>
+
         
-          </button>
-          </Link>  
-         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-             about
-         </button>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-             contact
-         </button>
           </div>
         </nav>
     );
